06ee5dcc76c128f76e4b3064c2501d7cc12ef582
AhRemy <aruem8888@gmail.com>
2022-09-16 10:18:09 +0900

    소스 수정 커밋

--- Scripts/clean_server.sh
+++ Scripts/clean_server.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
-###백업 등의 작업이 필요한 경우 shell script 작성
-##find /opt/codedeploy-agent/deployment-root/0819cead-1ac3-401c-be0c-7a57a5f92ae4/*-maxdepth 0 -type 'd' |grep -v $(stat -c '%Y:%n' /opt/codedeploy-agent/deployment-root/0819cead-1ac3-401c-be0c-7a57a5f92ae4/* | sort -t: -n | tail-1 | cut -d: -f2- | cut -c 3-) | xargs rm -rf
-##sudo rm -rf /home/ec2-user/codedeploy/*
+#! /bin/bash
+find /opt/codedeploy-agent/deployment-root/deployment-instructions/* -maxdepth 0 -type 'd' | grep -v
+$(stat -c '%Y:%n' /opt/codedeploy-agent/deployment-root/deployment-instructions/* | sort -t: -n | tail -1 | cut -d: -f2- | cut -c 3-) | xargs rm -rf
+sudo rm -rf /home/ec2-user/codedeploy/*
