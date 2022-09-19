0ea290626848ca796ded73882bc312a1de8dc322
AhRemy <aruem8888@gmail.com>
2022-09-19 15:07:57 +0900

    다른서버도 배포~~

--- Scripts/stop_server.sh
+++ Scripts/stop_server.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-sudo pkill -f 'java -jar'
+var1=$(ps -ef |grep java |grep test-0.0)
+echo process info:${var1}
+
+get_pid=$(echo ${var1} | cut -d " " -f2)
+
+if [ -n ${get_pid} ]
+
+then
+        result1=$(sudo kill -9 ${get_pid})
+        echo process is killed.
+else
+        echo running process not found.
+fi
