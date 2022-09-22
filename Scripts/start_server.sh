60eb4fc82a9d7bf9448aa2ecbd999df6e23fc5fe
AhRemy <aruem8888@gmail.com>
2022-09-22 11:01:27 +0900

    쉘스크립트 수정

--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
+. ./env.sh
+
+if [ ! -d "$LOG_HOME/ndump" ]; then
+    mkdir -p $LOG_HOME/nohup
+fi
+
+if [ -f $LOG_HOME/nohup/$INST_NAME.out ]; then
+    mv $LOG_HOME/nohup/$INST_NAME.out $LOG_HOME/nohup/$INST_NAME.$DATE
+fi
+
+export CATALINA_OUT=$LOG_HOME/nohup/$INST_NAME.out
 
 cd /home/ec2-user/codedeploy-test
-sudo java -jar *.jar > /dev/null 2> /dev/null < /dev/null &
+sudo java -jar *.jar >> /dev/null 2>&1 &
 
+
+PID=`ps -ef | grep java | grep test-0.0.1 | awk '{print $2}'`
+echo " +$PID"
