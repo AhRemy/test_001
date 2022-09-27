0d7e3b44953457b006f6e851738498ff114cc573
AhRemy <aruem8888@gmail.com>
2022-09-27 13:20:02 +0900

    commit

--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 cd /home/ec2-user/codedeploy-test
-#sudo java -jar *.war >> /dev/null 2>&1 &
-sudo java -jar *.jar >> /dev/null 2>&1 &
+sudo java -jar *.war >> /dev/null 2>&1 &
+
+
+PID=`ps -ef | grep java | grep test-0.0.1 | awk '{print $2}'`
+echo " +$PID"
