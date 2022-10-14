05062ebfbaddca3603c8709a448d2e0adf37b649
AhRemy <aruem8888@gmail.com>
2022-10-14 17:10:53 +0900

    원상태로 커밋

--- src/main/java/com/example/test/project/repository/MemoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.test.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.test.project.vo.Memo;
-
-public interface MemoRepository extends JpaRepository<Memo,Long>{
-
-}
